88288db540d10dd1c00bddf827acfb466d586223
Jorge <georgevazj@gmail.com>
2021-08-22 22:47:49 +0200

    Resource group name definition

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ resource "azurerm_key_vault" "akv" {
   enabled_for_disk_encryption = var.enabled_disk_encryption
   soft_delete_retention_days = var.soft_delete_retention_days
   purge_protection_enabled = var.purge_protection_enabled
+  resource_group_name = var.resource_group_name
 
   sku_name = var.sku_name
 

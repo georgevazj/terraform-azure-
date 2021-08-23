0f9190765778bca391cf8c6be6f8c73de71f2249
Jorge <georgevazj@gmail.com>
2021-08-23 17:19:36 +0200

    Skip provider registration

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
+  skip_provider_registration = true
   features {
     key_vault {
       purge_soft_delete_on_destroy = true
